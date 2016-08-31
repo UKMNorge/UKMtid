2f6a56f2d50893f2abb009d81217115e8bfc64bc
Asgeir Stavik Hustad <askhus@online.no>
2016-08-31 19:32:40 +0200

    Clickable div in month

--- web/js/month.jquery.js
+++ web/js/month.jquery.js
@@ -9,3 +9,18 @@ $("#monthChange").change(function() {
 	url = cur_url[0]+'/user-'+user_id+'/'+date;
 	location = url;
 });
+
+$(".interval_box").click(function(e) {
+	e.preventDefault();
+
+	//console.log($(this));
+	var interval_id = $(this).find('#interval_id').val();
+	//console.log(interval_id);
+
+	cur_url = ''+location;
+	cur_url = cur_url.split("/user");
+	//console.log(cur_url);
+	url = cur_url[0]+'/edit/'+interval_id;
+	
+	location = url;
+});
